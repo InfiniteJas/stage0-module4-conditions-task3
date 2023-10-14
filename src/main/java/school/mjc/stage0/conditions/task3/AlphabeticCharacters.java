839869f378b58b3a70b5b55707ad62e2869e3f06
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-15 00:13:34 +0600

    Module 4.1 task 3 - 2

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,14 +1,13 @@
 package school.mjc.stage0.conditions.task3;
 
-public class AlphabeticCharacters {
-    if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
-        // Check if the character is a vowel
+public void determineCharacterType(char character) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
         if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
             System.out.println("Vowel");
         } else {
             System.out.println("Consonant");
         }
-    } else {
-        System.out.println("wrong alphabet!");
-    }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
 }
